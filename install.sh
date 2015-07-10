a1599e59fd0c2de6ef25c5890da74e24c3034410
Somsubhra Bairi <somsubhra.b@endurance.com>
2015-07-10 16:52:04 +0530

    Fix install script

--- install.sh
+++ install.sh
@@ -1,6 +1,5 @@
 #!/bin/sh
 
-sudo apt-get install python-pil
-sudo apt-get install imagemagick
+sudo apt-get install python-pil imagemagick
 sudo curl https://raw.githubusercontent.com/Somsubhra/xkcd-cl/master/xkcdcl > /usr/bin/xkcdcl
 sudo chmod a+x /usr/bin/xkcdcl
